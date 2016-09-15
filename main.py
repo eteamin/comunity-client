6c33752c60898c9c79ffdd342955d9bab0426256
eteamin <aminetesamian1371@gmail.com>
2016-09-15 20:53:48 +0430

    question page logic ready

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 
 
 cached_questions = None
-selected_question = None
+question = None
 screen_manager = ScreenManager(transition=SlideTransition())
 
 
@@ -42,20 +42,42 @@ class MainScreen(Screen):
         self.add_widget(root)
 
     def select_question(self, *args):
-        global selected_question
-        selected_question = args[0]
+        global question
+        question = args[0]
         screen_manager.switch_to(QuestionScreen())
 
 
 class QuestionScreen(Screen):
-    if selected_question:
-        title = selected_question['title']
-        content = selected_question['content']
-        creation_time = selected_question['creation_time']
-        like = selected_question['like']
-        account_name = selected_question['account']['display_name']
-        tag = selected_question['tag']
-        answer = selected_question['answer']
+    def __init__(self):
+        super(QuestionScreen, self).__init__()
+        if question:
+            root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+
+            body = GridLayout(cols=1, spacing=2, size_hint_y=None)
+            body.bind(minimum_height=body.setter('height'))
+
+            question_container = RelativeLayout(size_hint=(1, None), size=(Window.width, Window.height / 2))
+            question_container.add_widget(Label(text=question['title'], pos_hint={'center_x': 0.1, 'center_y': 0.5}))
+            question_container.add_widget(
+                Label(text=question['creation_time'], pos_hint={'center_x': 0.8, 'center_y': 0.1})
+            )
+            question_container.add_widget(
+                Label(text=question['account']['display_name'], pos_hint={'center_x': 0.8, 'center_y': 0.2})
+            )
+            body.add_widget(question_container)
+
+            for a in question['answers']:
+                container = RelativeLayout(size_hint=(1, None), size=(Window.width, Window.height / 3))
+                container.add_widget(Label(text=a['content'], pos_hint={'center_x': 0.1, 'center_y': 0.5}))
+                container.add_widget(Label(text=a['like'], pos_hint={'center_x': 0.1, 'center_y': 0.5}))
+                container.add_widget(
+                    Label(text=a['account']['display_name'], pos_hint={'center_x': 0.8, 'center_y': 0.2})
+                )
+                container.add_widget(Label(text=a['creation_time'], pos_hint={'center_x': 0.8, 'center_y': 0.1}))
+                body.add_widget(container)
+
+            root.add_widget(body)
+            self.add_widget(root)
 
 
 class NotificationScreen(Screen):

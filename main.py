f01b02e5d734287878a532e6243a6632ecd80931
Amin Etesamian <aminetesamian1371@gmail.com>
2017-02-17 02:20:44 +0330

    clean up

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 from functools import partial
-from random import uniform as u
-from itertools import chain
 from os import path
 import json
 
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.graphics import Color, Rectangle, Line
-from kivy.lang import Builder
 from kivy.metrics import dp
-from kivy.properties import StringProperty, ReferenceListProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics.texture import Texture
-from kivy.uix.dropdown import DropDown
-from kivy.uix.filechooser import FileChooserIconView
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.widget import Widget
 from kivy.uix.image import AsyncImage
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
-from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.uix.textinput import TextInput
